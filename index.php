f87604fd1f8260ad57cc90f678e117f8b834345e
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-24 20:23:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+  session_start();
+
+  // If the session vars aren't set, try to set them with a cookie
+  if (!isset($_SESSION['user_id'])) {
+    if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
+      $_SESSION['user_id'] = $_COOKIE['user_id'];
+      $_SESSION['username'] = $_COOKIE['username'];
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Startupwala</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h3>Welcome to Startupwala!</h3>
+
+<?php
+ 
+  // Generate the navigation menu
+  if (isset($_SESSION['username'])) {
+    echo '&#10084; <a href="viewprofile.php">View Profile</a><br />';
+    echo '&#10084; <a href="editprofile.php">Edit Profile</a><br />';
+    echo '&#10084; <a href="logout.php">Log Out (' . $_SESSION['username'] . ')</a>';
+  }
+  else {
+    echo '&#10084; <a href="login.php">Log In</a><br />';
+    echo '&#10084; <a href="signup.php">Sign Up</a>';
+  }
+
+
+?>
+
+</body> 
+</html>
