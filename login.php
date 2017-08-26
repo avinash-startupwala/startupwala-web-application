028ac68205ef023dd64c02bd500aa45e9a12df30
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-26 11:37:44 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,90 +1,78 @@
 <?php
-  require_once('heroku_postgres_database.php');
-
-
-    
-
-  // Start the session
   session_start();
 
-  // Clear the error message
-  $error_msg = "";
-
-  // If the user isn't logged in, try to log them in
+  // If the session vars aren't set, try to set them with a cookie
   if (!isset($_SESSION['user_id'])) {
-    if (isset($_POST['submit'])) {
-      // Connect to the database
-
-      // Grab the user-entered log-in data
-    
-    $herokupostgrsdatabse = new HerokuPostgresDatabase();
-
-        $user_username =  $herokupostgrsdatabse->escape_value(trim($_POST['username']));
-
-      $user_password =  $herokupostgrsdatabse->escape_value(trim($_POST['password']));
-      
-      if (!empty($user_username) && !empty($user_password)) {
-        // Look up the username and password in the database
-        $query = "SELECT user_id, email FROM registered_users WHERE email = '$user_username' AND password = '$user_password'";
-        
-
-        $data = $herokupostgrsdatabse->query($query);
-
-        if (pg_num_rows($data) == 1) {
-          // The log-in is OK so set the user ID and username session vars (and cookies), and redirect to the home page
-          $row = $herokupostgrsdatabse->fetch_array($data);
-          $_SESSION['user_id'] = $row['user_id'];
-          $_SESSION['username'] = $row['email'];
-          setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
-          setcookie('username', $row['email'], time() + (60 * 60 * 24 * 30));  // expires in 30 days
-          $home_url = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
-          header('Location: ' . $home_url);
-        }
-        else {
-          // The username/password are incorrect so set an error message
-          $error_msg = 'Sorry, you must enter a valid username and password to log in.';
-        }
-      }
-      else {
-        // The username/password weren't entered so set an error message
-        $error_msg = 'Sorry, you must enter your username and password to log in.';
-      }
+    if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
+      $_SESSION['user_id'] = $_COOKIE['user_id'];
+      $_SESSION['username'] = $_COOKIE['username'];
     }
   }
 ?>
 
 <!DOCTYPE html>
-<html>
 <head>
-  <title>Startupwala - Log In</title>
+  <title>Startupwala - View Profile</title>
 </head>
 <body>
-  <h3>Startupwala - Log In</h3>
+  <h3>Startupwala - View Profile</h3>
 
 <?php
-  // If the session var is empty, show any error message and the log-in form; otherwise confirm the log-in
-  if (empty($_SESSION['user_id'])) {
-    echo '<p class="error">' . $error_msg . '</p>';
-?>
+ 
 
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <fieldset>
-      <legend>Log In</legend>
-      <label for="username">Username:</label>
-      <input type="text" name="username" value="<?php if (!empty($user_username)) echo $user_username; ?>" /><br />
-      <label for="password">Password:</label>
-      <input type="password" name="password" />
-    </fieldset>
-    <input type="submit" value="Log In" name="submit" />
-  </form>
+	require_once("heroku_postgres_database.php");
 
-<?php
+  // Make sure the user is logged in before going any further.
+  if (!isset($_SESSION['user_id'])) {
+    echo '<p class="login">Please <a href="login.php">log in</a> to access this page.</p>';
+    exit();
   }
   else {
-    // Confirm the successful log-in
-    echo('<p class="login">You are logged in as ' . $_SESSION['username'] . '.</p>');
+    echo('<p class="login">You are logged in as ' . $_SESSION['username'] . '. <a href="logout.php">Log out</a>.</p>');
   }
-?>
 
-</body>
+  // Connect to the database
+	$herokupostgrsdatabse = new HerokuPostgresDatabase();
+  // Grab the profile data from the database
+  if (!isset($_GET['user_id'])) {
+    // $query = "SELECT username, first_name, last_name, gender, birthdate, city, state, picture FROM mismatch_user WHERE user_id = '" . $_SESSION['user_id'] . "'";
+
+
+
+$fetch_user_data=  "SELECT  first_name, last_name, email, phone,city, looking_for FROM registered_users WHERE user_id = '" . $_SESSION['user_id'] . "'";
+
+
+  }
+  else {
+    $fetch_user_data = "SELECT  first_name, last_name, email, phone,city, looking_for FROM registered_users WHERE user_id = '" . $_GET['user_id'] . "'";
+  }
+  	$fetch_user_data_result =  $herokupostgrsdatabse->query($fetch_user_data);
+
+  	if(pg_num_rows($fetch_user_data_result) < 1)
+  	{
+
+  			echo "<br>";
+  		echo "<h2>Sorry No data available in salesforce with username {$user}</h2>";
+  	}
+
+			
+	while( $row = pg_fetch_array($fetch_user_data_result))
+			   {
+
+			   	 print "\n";
+
+
+			   	 ?>
+			   	 <pre>
+			   	 <?php
+			 		print_r($row);
+			   	 ?>
+			   	 </pre>
+			   	 <?php
+
+
+			   }
+
+?>
+</body> 
 </html>
