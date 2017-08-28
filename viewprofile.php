dac07dd775bbe4522d37d1b22fd00d6da03f66ae
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-28 11:42:49 +0530

    Update viewprofile.php

--- viewprofile.php
+++ viewprofile.php
@@ -44,7 +44,7 @@ $fetch_user_data=  "SELECT  first_name, last_name, email, phone,city, looking_fo
 
   }
   else {
-    $fetch_user_data = "SELECT  first_name, last_name, email, phone,city, looking_for FROM registered_users WHERE user_id = '" . $_GET['user_id'] . "'";
+    $fetch_user_data = "SELECT  user_id, first_name, last_name, email, phone,city, looking_for FROM registered_users WHERE user_id = '" . $_GET['user_id'] . "'";
   }
   	$fetch_user_data_result =  $herokupostgrsdatabse->query($fetch_user_data);
 
